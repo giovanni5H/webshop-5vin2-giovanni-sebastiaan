6d1b93437908b029f99990aa685f76c99ad4e791
Sebasvwo <112023@emmauscollege.nl>
2021-12-24 19:34:56 +0000

    2/3 stap 1

--- db/create.sql
+++ db/create.sql
@@ -21,10 +21,10 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Arsenal', 'Het Arsenal thuisshirt van 2021/2022, '816905633-0', 69.9);
-insert into products (name, description, code, price) values ('Chelsea', 'Het Chelsea thuisshirt van 2021/2022, '077030122-3', 69.9);
-insert into products (name, description, code, price) values ('Liverpool', 'Het Liverpool thuisshirt van 2021/2022, '445924201-X', 69.9);
-insert into products (name, description, code, price) values ('Man United', 'Het Manchester United thuisshirt van 2021/2022, '693155505-7', 69.9);
-insert into products (name, description, code, price) values ('Man City', 'Het Manchester City thuisshirt van 2021/2022, '686928463-6', 69.9);
-insert into products (name, description, code, price) values ('Tottenham Hotspur', 'Het Tottenham Hotspur thuisshirt van 2021/2022, '492662523-7', 69.9);
+insert into products (name, description, code, price) values ('Arsenal', 'Het Arsenal thuisshirt van 2021/2022', '816905633-0', 69.9);
+insert into products (name, description, code, price) values ('Chelsea', 'Het Chelsea thuisshirt van 2021/2022', '077030122-3', 69.9);
+insert into products (name, description, code, price) values ('Liverpool', 'Het Liverpool thuisshirt van 2021/2022', '445924201-X', 69.9);
+insert into products (name, description, code, price) values ('Man United', 'Het Manchester United thuisshirt van 2021/2022', '693155505-7', 69.9);
+insert into products (name, description, code, price) values ('Man City', 'Het Manchester City thuisshirt van 2021/2022', '686928463-6', 69.9);
+insert into products (name, description, code, price) values ('Tottenham Hotspur', 'Het Tottenham Hotspur thuisshirt van 2021/2022', '492662523-7', 69.9);
 
